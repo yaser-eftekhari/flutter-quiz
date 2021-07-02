581fee83423588262a9cd916387cd0370e18794c
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-01 22:50:29 -0400

    using getter in result widget. Also some styling for it

--- lib/result.dart
+++ lib/result.dart
@@ -6,16 +6,36 @@ class Result extends StatelessWidget {
 
   Result({required this.totalScore, required this.resetAction});
 
+  String get textResult {
+    if (totalScore < 10) return "We are not alike!";
+    if (totalScore < 20) return "We are kind of similar";
+
+    return "We are alike!";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          "Your score is $totalScore",
-          style: TextStyle(fontSize: 25),
-        ),
-        ElevatedButton(onPressed: resetAction, child: Text("Reset Quiz")),
-      ],
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      width: 350,
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Text(
+            "Your score is $totalScore",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            textResult,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          ElevatedButton(onPressed: resetAction, child: Text("Reset Quiz")),
+        ],
+      ),
     );
   }
 }
